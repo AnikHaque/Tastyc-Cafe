c67df3efc7b223249c1507a4a7322d3ede49dc72
AnikHaque <ekramul15-811@diu.edu.bd>
2026-02-11 18:00:51 +0600

    menu model done

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Food(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='foods')
+    name = models.CharField(max_length=150)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    image = models.ImageField(upload_to='foods/')
+    is_available = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
