2d7c064b2e83c180ffd4aa8dad04029a2e6670f8
AnikHaque <ekramul15-811@diu.edu.bd>
2026-02-15 15:17:16 +0600

    blog model done

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.text import slugify
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True, blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    thumbnail = models.ImageField(upload_to='blogs/thumbnails/')
+    content = models.TextField()
+    category = models.CharField(max_length=100, default='Culinary Tips')
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_published = models.BooleanField(default=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
