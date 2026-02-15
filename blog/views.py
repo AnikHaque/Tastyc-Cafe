673f859df2191b168cedfae668aa7b27a3da0edf
AnikHaque <ekramul15-811@diu.edu.bd>
2026-02-15 15:23:09 +0600

    blog view done

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
 
-# Create your views here.
+# All Blogs List Page
+def blog_list(request):
+    blogs = Blog.objects.filter(is_published=True).order_by('-created_at')
+    return render(request, 'blog/blog_list.html', {'blogs': blogs})
+
+# Blog Details Page
+def blog_detail(request, slug):
+    # Slug diye specific blog khuje ber kora hobe
+    blog = get_object_or_404(Blog, slug=slug)
+    # Related blogs (optional: niche show koranor jonno)
+    related_blogs = Blog.objects.filter(is_published=True).exclude(id=blog.id)[:3]
+    return render(request, 'blog/blog_detail.html', {
+        'blog': blog,
+        'related_blogs': related_blogs
+    })
